b1273ea9c324a09d01983438d71882de0c0de325
yuqi-lee <52791037+yuqi-lee@users.noreply.github.com>
2020-05-18 16:22:44 +0800

    增加了一些注释

--- lab2/my_func.c
+++ lab2/my_func.c
@@ -1,5 +1,7 @@
 #include<asm/segment.h>
 
+/*新增的两个系统调用代码*/
+
 int sys_print_val(int a)
 {
 	printk("in sys_print_val: %d\n",a);
@@ -13,13 +15,13 @@ int sys_str2num(char *str, int str_len, long *ret)
 	long t;
 	t = 0; 
 	for(i = 0; i < str_len ; i++)
-	{
-		n = get_fs_byte(str + i);
+	{	
+		n = get_fs_byte(str + i);	/*从用户态传递数据至内核*/
 		if( n >= '0' && n <= '9')
 		{
 			t = t*10 + n - '0';
 		}
 	}
-	put_fs_long(t,ret);
+	put_fs_long(t,ret);	/*数据传递回用户态*/
 	return(0);
 }
